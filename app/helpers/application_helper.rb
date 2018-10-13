9512cda4feaf012e8d44fdcabd04736ab5a5edd7
jonnyboy11494 <jonathan.yu.94@gmail.com>
2018-10-13 17:08:02 +1100

    Finished template for home, about, contact

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,21 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.       # Documentation comment
   def full_title(page_title = '')                     # Method def, optional arg
-    base_title = "#100DaysofCode"  # Variable assignment
+    base_title = "JonNixon"  # Variable assignment
     if page_title.empty?                              # Boolean test
       base_title                                      # Implicit return
     else
       page_title + " | " + base_title                 # String concatenation
     end
+  end
+
+  # Returns background image url
+  def bg_url(bg_img = '')
+    if bg_img.empty?
+      bg_img = "home-bg.jpg"
+    else
+      bg_img = bg_img.downcase() + "-bg.jpg"
+    end
+    image_path(bg_img)
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "JonNixon"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
